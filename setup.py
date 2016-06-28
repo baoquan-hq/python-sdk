a2346ed4f23e1489d61175c4140720a5b30a3615
sbwdlihao <sbwdlihao@gmail.com>
2016-06-28 11:02:32 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='eagle-python-sdk',
     version='1.0.0',
-    packages=['src.main.util'],
+    packages=find_packages(),
     url='https://baoquan.com',
     license='gpl-3.0',
     author='sbwdlihao',
     author_email='sbwdlihao@gmail.com',
-    description='sdk for members to use the service of baoquan.com'
+    keywords='baoquan eagle sdk',
+    description='sdk for members to use the service of baoquan.com',
+    long_description=open('README.md').read(),
+    install_requires=['rsa==3.4.2', 'requests==2.10.0'],
+    tests_require=['fake-factory==0.5.7'],
+    include_package_data=True
 )
